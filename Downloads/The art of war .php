fc98de302c1781ed685fe81956cb5c0334c392b0
Suyog <suyogpatil58@gmail.com>
2024-01-31 19:54:17 +0530

    New changes

---
+++ Downloads/The art of war .php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en" itemscope itemtype="http://schema.org/Book">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+
+	<title>The Art of War by Sun Tzu - Download</title>
+
+    <meta name="robots" content="NOODP">
+    <meta name="description" content="Download The Art of War by Sun Tzu in PDF or plain text to read on your mobile device, computer, or print.">
+
+	<meta name="google-site-verification" content="4uD54xDb_CoV5yxVovVJKFlJAD5619ax6Ng7ss0xhgc" />
+
+	<meta property="og:title" content="The Art of War by Sun Tzu - Download">
+	<meta property="og:description" content="Download The Art of War by Sun Tzu in PDF or plain text to read on your mobile device, computer, or print.">
+	<meta property="og:image" content="http://suntzusaid.com/images/heading.png">
+
+    <link href="https://fonts.googleapis.com/css?family=Cormorant+Infant:400,400i,700,700i|Lato:400,400i,700,700i&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="/css/main.css" media="screen">
+	<link rel="stylesheet" type="text/css" href="/css/print.css" media="print">
+	<link rel="apple-touch-icon" href="/images/apple-touch-icon.png">
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+	<script type="text/javascript" src="/js/suntzusaid.js"></script>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3778874774376597"
+     crossorigin="anonymous"></script></head>
+<body>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-53154-14"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-53154-14', { 'anonymize_ip': true });
+</script>
+
+<div id="header">
+	<h1><a href="/"><img src="/images/heading.png" alt="Sun Tzu's The Art of War"></a></h1>
+	<div class="social">
+		<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://suntzusaid.com" data-text="Sun Tzu Said" data-via="yafd">Tweet</a>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+	</div>
+</div>
+
+<div id="wrap">
+
+<div id="content">
+	
+<div class="download-ad">
+    </div>
+
+<h3>Before you download...</h3>
+
+<p>
+    Did you know you can read the entire book online for free right now?
+    <a href="/">Click here to start reading online at Chapter 1...</a>
+</p>
+
+<p>
+    Try our <a href="/index">search page</a> if you're looking for something
+    specific or want to research a specific topic. And for an overview, click
+    over to the <a href="/">table of contents.</a>
+</p>
+
+<div id="downloadpdf" class="download">
+	<h3>Download PDF</h3>
+
+	<p>Click to download a PDF version of the text.</p>
+
+    <form action="/download.php" method="post" id="download-pdf">
+        <input type="hidden" name="file" value="pdf">
+        <input type="submit" value="Download PDF">
+    </form>
+</div>
+
+<div id="downloadtext" class="download">
+	<h3>Download Plain text</h3>
+
+	<p>Click to download a plain text version of the text.</p>
+
+    <form action="/download.php" method="post" id="download-txt">
+        <input type="hidden" name="file" value="txt">
+        <input type="submit" value="Download plain text">
+    </form>
+</div>
+
+<div class="clear"></div>
+
+<p style="margin-top: 25px"><a href="/">Return to Table of Contents &raquo;</a></p>
+
+<script>
+$(function() {
+    $('#download-pdf').submit(function() {
+        _trackEvent('PDF', 'Download');
+        return true;
+    });
+    $('#download-txt').submit(function() {
+        _trackEvent('TXT', 'Download');
+        return true;
+    });
+});
+</script>
+
+</div> <!-- content -->
+
+</div> <!-- wrap -->
+
+<div id="footer">
+	<p>
+		Copyright &copy; 2023 <a href="http://watson-net.com">SunTzuSaid.com</a>. All Rights Reserved.
+	</p>
+
+	<div style="padding: 10px 0" class="floatleft alignleft">
+			</div>
+</div>
+
+</body>
+</html>
